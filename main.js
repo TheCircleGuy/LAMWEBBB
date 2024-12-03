3ba9c6444ffe07a039be73f02afc42571a050cd3
TheCircleGuy <92421392+TheCircleGuy@users.noreply.github.com>
2024-12-04 03:23:36 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
+console.log("JAVSCRIPT RUNNING")
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".small-card");
+  let activeIndex = 0;
 
-  const navbar = document.querySelector('.navbar');
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
-      navbar.classList.add('scrolled'); // Add the class when scrolling down
-    } else {
-      navbar.classList.remove('scrolled'); // Remove the class when at the top
-    }
-  });
+  const rotateCards = () => {
+    // Remove active class from all cards
+    cards.forEach((card) => card.classList.remove("active"));
+
+    // Add active class to the current card
+    cards[activeIndex].classList.add("active");
+
+    // Increment the index
+    activeIndex = (activeIndex + 1) % cards.length;
+  };
+
+  // Rotate cards every 3 seconds
+  setInterval(rotateCards, 3000);
+});
+
+VanillaTilt.init(document.querySelectorAll(".box"), {
+  max: 25,
+  speed: 400,
+  glare: true,
+  "max-glare":0.5
+});
